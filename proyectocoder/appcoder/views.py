e4f32d6caa5fb5901e233b4223ecde8fa18cfc0b
Israel Jimenez <israel@snfmex.com.mx>
2022-11-16 16:48:37 -0600

    FEATURE: Formularios busqueda e ingreso de datos

--- proyectocoder/appcoder/views.py
+++ proyectocoder/appcoder/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from appcoder.models import Curso
+from appcoder.models import Curso, Profesor
+from appcoder.forms import ProfesorFormulario
 
 # Create your views here.
 
@@ -14,15 +15,61 @@ def cursos(request):
     for curso in cursos:
         print(curso.nombre)
     return render(request, "appcoder/cursos.html")
+
+def creacion_curso(request):
+
+    if request.method == "POST":
+        nombre_curso = request.POST["curso"]
+        numero_camada = request.POST["camada"]
+
+        curso = Curso(nombre=nombre_curso, camada=numero_camada)
+
+        curso.save()
+
+    return render(request, "appcoder/curso_formulario.html")
+
+def buscar_curso(request):
+    
+    return render(request, "appcoder/busqueda_cursos.html")
+
+
+def resultado_busqueda_cursos(request):
+    nombre_curso = request.GET["nombre_curso"]
+
+    cursos = Curso.objects.filter(nombre__icontains=nombre_curso)
+    return render(request, "appcoder/resultados_busqueda_cursos.html",{"cursos":cursos})
+
     
 def estudiantes(request):
     return render(request, "appcoder/estudiantes.html")
     
 def profesores(request):
     return render(request, "appcoder/profesores.html")
+
+def creacion_profesores(request):
+
+    if request.method == "POST":
+        formulario = ProfesorFormulario(request.POST)
+
+        #Validamos que el formulario no tenga problemas
+        if formulario.is_valid():
+            #Recuperamos los datos del atributo cleaned_data
+            data = formulario.cleaned_data
+
+            profesor = Profesor(nombre=data["nombre"], apellido = data["apellido"], email = data["email"], profesion = data["profesion"])
+
+            profesor.save()
+        
+
+    formulario = ProfesorFormulario()
+
+    contexto = {"formulario": formulario}
+
+    return render(request, "appcoder/profesores_formulario.html", contexto)
     
 def entregables(request):
     return render(request, "appcoder/entregables.html")
+
 
 
 # def listado_cursos(request):
